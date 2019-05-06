fee8b20b793546eeaba40c65e2af435d889d48d8
Sebastiaan Mannem <sebastiaan.mannem@enterprisedb.com>
2019-05-07 01:50:53 +0200

    Added option for prepared, transactional and prep_trans, and added options for different queries.

--- pg_cpu_load/src/main.rs
+++ pg_cpu_load/src/main.rs
@@ -93,32 +93,93 @@ fn parse_args() -> Result<args::Args, args::ArgsError> {
         "PGUSER",
         Occur::Optional,
         None);
-
+    args.option("t",
+        "query_type",
+        "The type of query to run: empty, simple, read, write",
+        "QTYPE",
+        Occur::Optional,
+        Some("simple".to_string()));
+    args.option("s",
+        "statement_type",
+        "The type of statwement prep to use: direct, prepared, transactional, prepared_transactional",
+        "STYPE",
+        Occur::Optional,
+        Some("direct".to_string()));
     args.parse(input)?;
 
     Ok(args)
 }
 
-fn thread(thread_id: u32, connect_string: String, tx: mpsc::Sender<f32>) -> Result<(), args::ArgsError>{
+fn thread(thread_id: u32, tx: mpsc::Sender<f32>) -> Result<(), Box<std::error::Error>>{
     println!("Thread {} started", thread_id);
-    let conn = Connection::connect(connect_string, TlsMode::None).unwrap();
+    let args = parse_args()?;
+
+    let qtype: String = args.value_of("query_type").unwrap();
+    let query: String;
+    match qtype.as_ref() {
+        "empty" => query = "".to_string(),
+        "simple" => query = "SELECT $1".to_string(),
+        "read" => query = "SELECT ID from my_temp_table WHERE ID = $1".to_string(),
+        "write" => query = "UPDATE my_temp_table set ID = $1 WHERE ID = $1".to_string(),
+        _ => panic!("Option QTYPE should be one of empty, simple, read, write (not {}).", qtype),
+    }
+
+    let stype: String = args.value_of("statement_type")?;
+    if stype != "prepared" && stype != "prepared_transactional" && stype != "transactional" && stype != "direct" {
+        panic!("Option STYPE should be one of direct, prepared, transactional, prepared_transactional (not {}).", stype);
+    }
+    if qtype == "empty" && stype != "prepared_transactional" && stype != "transactional" {
+        panic!("Option QTYPE-empty only works with transactions.");
+    }
+
+    let connect_string = postgres_connect_string(args);
+    if thread_id == 0 {
+        println!("Connectstring: {}", connect_string);
+        println!("Query: {}", query);
+        println!("SType: {}", stype);
+    }
+    let conn = Connection::connect(connect_string, TlsMode::None)?;
     let mut tps: u64 = 1000;
+
+    if qtype == "read" || qtype == "write" {
+        conn.execute("create temporary table my_temp_table (id oid)", &[])?;
+        conn.execute("insert into my_temp_table values($1)", &[&thread_id])?;
+    }
+
     loop {
         let start = SystemTime::now();    
         for _x in (1..tps).rev() {
-            let _row = &conn.query("SELECT 1", &[]).unwrap();
+            if stype == "prepared" {
+                let prep = conn.prepare_cached(&query)?;
+                let _row = prep.query(&[&thread_id]);
+            } else if stype == "transactional" {
+                let trans = conn.transaction()?;
+                if qtype != "empty" {
+                    let _row = trans.query(&query, &[&thread_id]);
+                }
+                let _res = trans.commit()?;
+            } else if stype == "prepared_transactional" {
+                let trans = conn.transaction()?;
+                if qtype != "empty" {
+                    let prep = trans.prepare_cached(&query)?;
+                    let _row = prep.query(&[&thread_id]);
+                }
+                let _res = trans.commit()?;
+            } else if qtype != "empty" {
+                let _row = &conn.query(&query, &[&thread_id]);
+            }
         }
-        let end = SystemTime::now();    
+        let end = SystemTime::now();
         let duration_nanos = end.duration_since(start)
             .expect("Time went backwards").as_nanos();
         let calc_tps = 10.0_f32.powi(9) * tps as f32 / duration_nanos as f32;
-        tx.send(calc_tps).unwrap();
+        tx.send(calc_tps)?;
         tps = calc_tps as u64;
     }
     //Ok(())
 }
 
-fn main() -> Result<(), args::ArgsError>{
+fn main() -> Result<(), Box<std::error::Error>> {
     let mut sum_tps: f32;
     let mut avg_tps: f32;
     let args = parse_args()?;
@@ -128,28 +189,25 @@ fn main() -> Result<(), args::ArgsError>{
         process::exit(0);
     }
 
-    let num_threads: String = args.value_of("parallel").unwrap();
-    let num_threads = u32::from_str(&num_threads).unwrap();
-
-    let connect_string = postgres_connect_string(args);
-    println!("Connectstring: {}", connect_string);
+    let num_threads: String = args.value_of("parallel")?;
+    let num_threads = u32::from_str(&num_threads)?;
 
     let (tx, rx) = mpsc::channel();
     for thread_id in (0..num_threads).rev() {
         let thread_tx = tx.clone();
-        let thread_connstr = connect_string.clone();
         thread::spawn(move || {
-            thread(thread_id, thread_connstr, thread_tx).unwrap();
+            thread(thread_id, thread_tx).unwrap();
         });
     }
 
     loop {
         sum_tps = 0_f32;
         for _thread_id in (0..num_threads).rev() {
-             sum_tps += rx.recv().unwrap();
+             sum_tps += rx.recv()?;
         }
         avg_tps = sum_tps / num_threads as f32;
         println!("Average tps: {}", avg_tps);
+        println!("Total tps: {}", sum_tps);
     }
     //Ok(())
 }
